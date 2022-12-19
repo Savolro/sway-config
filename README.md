c08410481fb710b40e5d1bf68c04fc04e93fb4a5
Savolro <borlovas@gmail.com>
2022-12-19 17:08:47 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Savolro's sway config
+This configuration includes pretty much the whole desktop configuration I use except. If you are
+interested in more configs I use:
+* [NeoVim config](https://github.com/Savolro/vim-config)
+
+## Installation
+### Arch
+``` sh
+sh <(curl -s https://raw.githubusercontent.com/Savolro/sway-config/main/install.sh)
+```
+
+### Others
+take a look at `install.sh` and do analigical steps.
--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Keep in mind this script is very primitive and has no checks whatsoever. It will mostly not
+# break your system but in case of failure (e. g. some directory exists) will leave everything
+# it was able to do already.
 set -eux
 
 # Install main dependencies
@@ -7,12 +10,45 @@ sudo pacman -S --needed swaybg alacritty dunst waybar wofi
 
 sudo pacman -S --needed git openssh git base-devel
 
-# # Install yay
-# git clone https://aur.archlinux.org/yay.git
-# cd yay
-# makepkg -si
-# cd ..
-# rm -rf yay
+# Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
 
 # Install extras from AUR
 yay -S --needed swaylock-effects wlogout
+
+# Install additional used tools
+sudo pacman -S --needed pavucontrol pulseaudio pulseaudio-alsa brightnessctl playerctl slurp grim wl-clipboard
+
+# Install necessary fonts
+mkdir -p ~/.local/share/fonts
+mkdir tmp
+cd tmp
+curl https://fonts.google.com/download?family=Space%20Grotesk -o SpaceGortesk.zip
+unzip SpaceGortesk.zip
+cp static/* ~/.local/share/fonts
+cd ..
+rm -rf tmp
+fc-cache
+sudo pacman -S --needed unzip ttf-font-awesome
+yay -S --needed ttf-adobe-source-code-pro-fonts 
+
+# TODO: check SSH/HTTPS
+mkdir -p ~/.config
+
+# Install the config itself
+git clone https://github.com/Savolro/sway-config.git ~/.config/sway
+ln -s ~/.config/sway/alacritty ~/.config/alacritty
+ln -s ~/.config/sway/dunst ~/.config/dunst
+ln -s ~/.config/sway/swaylock ~/.config/swaylock
+ln -s ~/.config/sway/waybar ~/.config/waybar
+ln -s ~/.config/sway/wlogout ~/.config/wlogout
+ln -s ~/.config/sway/wofi ~/.config/wofi
+
+# Set GTK theme and fond for GTK apps
+yay -S --needed mojave-gtk-theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Mojave-Dark'
+gsettings set org.gnome.desktop.interface font-name 'Space Grotesk 11'
