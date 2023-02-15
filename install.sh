28cac6c61b887f90f9ba53827a8d8cd162e0d847
Savolro <borlovas@gmail.com>
2023-02-16 00:00:41 +0200

    Replace dunst with swaync

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ set -eux
 
 # Install main dependencies
 sudo pacman -S --needed sway polkit
-sudo pacman -S --needed swaybg alacritty dunst waybar wofi
-
+sudo pacman -S --needed swaybg alacritty waybar wofi
 sudo pacman -S --needed git openssh git base-devel
+sudo pacman -S --needed python-pip
+
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git
@@ -17,8 +18,16 @@ makepkg -si
 cd ..
 rm -rf yay
 
+# Install autotiling
+pip install i3ipc
+git clone https://github.com/nwg-piotr/autotiling
+mkdir -p ~/bin
+mv autotiling/autotiling/main.py ~/bin/autotiling
+chmod +x ~/bin/autotiling
+rm -rf autotiling
+
 # Install extras from AUR
-yay -S --needed swaylock-effects wlogout
+yay -S --needed swaylock-effects wlogout swaync
 
 # Install additional used tools
 sudo pacman -S --needed pavucontrol pulseaudio pulseaudio-alsa brightnessctl playerctl slurp grim wl-clipboard
@@ -34,6 +43,7 @@ cd ..
 rm -rf tmp
 fc-cache
 sudo pacman -S --needed unzip ttf-font-awesome
+sudo pacman -S --needed unzip noto-fonts
 yay -S --needed ttf-adobe-source-code-pro-fonts 
 
 # TODO: check SSH/HTTPS
@@ -42,13 +52,13 @@ mkdir -p ~/.config
 # Install the config itself
 git clone https://github.com/Savolro/sway-config.git ~/.config/sway
 ln -s ~/.config/sway/alacritty ~/.config/alacritty
-ln -s ~/.config/sway/dunst ~/.config/dunst
 ln -s ~/.config/sway/swaylock ~/.config/swaylock
 ln -s ~/.config/sway/waybar ~/.config/waybar
 ln -s ~/.config/sway/wlogout ~/.config/wlogout
 ln -s ~/.config/sway/wofi ~/.config/wofi
+ln -s ~/.config/sway/swaync ~/.config/swaync
 
 # Set GTK theme and fond for GTK apps
-yay -S --needed mojave-gtk-theme
-gsettings set org.gnome.desktop.interface gtk-theme 'Mojave-Dark'
+yay -S --needed layan-gtk-theme-git
+gsettings set org.gnome.desktop.interface gtk-theme 'Layan-Dark'
 gsettings set org.gnome.desktop.interface font-name 'Space Grotesk 11'
